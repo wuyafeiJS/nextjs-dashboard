9d6f6fea100acda23ac33ca02ca8251a7593093d
wuyf <wuyf@mingyuanyun.com>
2025-08-28 15:14:41 +0800

    feat: done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import { inter } from '@/app/ui/fonts';
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme',
+    default: 'Acme',
+  },
+  description: 'The official Next.js Learn Course',
+};
 
 export default function RootLayout({
   children,
